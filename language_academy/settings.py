94c87c72e2694be6a8e879193e3afcf3a33396ba
Elena Bychkova <lena.bychkova.200012@gmail.com>
2021-12-08 18:31:14 +0300

    DEV: react app is created

--- language_academy/settings.py
+++ language_academy/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'language_academy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'la-frontend/build'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,6 +122,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+STATICFILES_DIRS = (
+    BASE_DIR / 'la-frontend/build/static',
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

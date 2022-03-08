c54e0464d00ac7587c8322c06fac6d31e02f2c2f
Elena Bychkova <lena.bychkova.200012@gmail.com>
2022-03-08 20:00:21 +0300

    Tokens: simplejwt was added

--- language_academy/urls.py
+++ language_academy/urls.py
@@ -1,12 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path
-from django.shortcuts import render
-from django.urls.conf import include, re_path
-from django.views.generic import TemplateView
+from django.urls.conf import include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 
-# def index_view(request):
-#     return render(request, 'build/index.html')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,7 +15,10 @@ urlpatterns = [
     path('users/', include("users.urls")),
     path('core/', include("core.urls")),
     path('api-auth/', include("rest_framework.urls")),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('', include('frontend.urls')),
 ]
 
-# urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
+

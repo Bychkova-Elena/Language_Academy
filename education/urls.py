4501fcd2a705eda88874798579f5baf753f2ad24
Elena Bychkova <lena.bychkova.200012@gmail.com>
2022-04-11 19:36:05 +0300

    Group: try to make additing group

--- education/urls.py
+++ education/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import TeachersCorseView, GetHomeworkView, GetTimeTableView, UpdateDeleteCorseView
+from .views import TeachersCourseView, GetHomeworkView, GetTimeTableView, UpdateDeleteCorseView 
 
 urlpatterns = [
 #     path('<int:pk>/timetable/',
 #          views.TimeTableView.as_view({'get': 'list'})),
 #     path('<int:pk>/homework/',
 #          views.HomeworkView.as_view({'get': 'list'})),
-    path('courses', TeachersCorseView.as_view()),
+    path('courses', TeachersCourseView.as_view()),
     path('courses/<int:courseId>', UpdateDeleteCorseView.as_view()),
     path('<int:pk>/gettimetable', GetTimeTableView.as_view()),
     path('<int:pk>/gethomework', GetHomeworkView.as_view())

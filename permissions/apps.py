ca151051c05ef717f5718a846e1e4f5653a1b095
Vladislav Bykovskii <vladislav_bykovskii@epam.com>
2022-04-25 23:56:41 +0300

    dev

--- permissions/apps.py
+++ permissions/apps.py
@@ -1,9 +1,7 @@
 from django.apps import AppConfig
 
+
 class PermissionsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'permissions'
     verbose_name = 'Права'
-    
-    def ready(self):
-       import permissions.signal

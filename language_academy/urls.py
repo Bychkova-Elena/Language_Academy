8a62ac4ef7584d6b2282b108ddb586a3d2398f81
Elena Bychkova <lena.bychkova.200012@gmail.com>
2022-05-13 20:13:29 +0300

    Homework: docs was created

--- language_academy/urls.py
+++ language_academy/urls.py
@@ -1,13 +1,22 @@
-
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from django.views.generic import TemplateView
+from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include("education.urls")),
     path('api/v1/', include("languages.urls")),
     path('api/v1/', include("users.urls")),
+    path('api_schema/', get_schema_view(
+        title='API Schema',
+        description='Guide for the REST API'
+    ), name='api_schema'),
+    path('docs/', TemplateView.as_view(
+        template_name='docs.html',
+        extra_context={'schema_url':'api_schema'}
+        ), name='swagger-ui'),
     path('api/v1/auth/', include("core.urls")),
     path('api-auth/', include("rest_framework.urls")),
     path('djoser/', include('djoser.urls')),
